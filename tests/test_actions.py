9f2122497983bfb57a9f2c1dcad782e3b00594bd
Sanhe <MacHu-GWU@users.noreply.github.com>
2021-12-07 21:30:03 -0500

    Update test_actions.py

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -26,26 +26,24 @@ class TestActions(object):
         #
         # in order to recovery the virtualenv again, we have to run
         # venv_up, pip_dev_install, req_test to recover it back to the correct state
-        actions.venv_remove(config)
-        actions.venv_up(config)
-        actions.pip_dev_install(config)
-        actions.req_test(config)
-        actions.req_dev(config)
-        actions.req_doc(config)
-        actions.req_info(config)
+        if "CI" not in os.environ:
+            actions.venv_remove(config)
+            actions.venv_up(config)
+            actions.pip_dev_install(config)
+            actions.req_test(config)
+            actions.req_dev(config)
+            actions.req_doc(config)
+            actions.req_info(config)
 
-        # actions.build_doc(config)
-        actions.info(config)
-        actions.build_lambda_source_code(config)
+            # actions.build_doc(config)
+            actions.info(config)
+            actions.build_lambda_source_code(config)
 
 
-        # caution! don't clean up tox dir, if you are testing this in
-        # tox environment, it will disturb other py version testing.
-        actions.clean_doc(config)
-        # actions.clean(config, ignore_tox=True)
-
-        # if "CI" not in os.environ:
-        #     actions.upload_lambda_source_code(config)
+            # caution! don't clean up tox dir, if you are testing this in
+            # tox environment, it will disturb other py version testing.
+            # actions.clean_doc(config)
+            # actions.clean(config, ignore_tox=True)
 
         if ("TOX_WORK_DIR" not in os.environ) and ("CI" not in os.environ):
             actions.upload_lambda_source_code(config)
