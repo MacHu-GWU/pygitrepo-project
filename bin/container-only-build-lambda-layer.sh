6e3a48ebdfcd30fa044ab679f5a03b5a98c1e728
Sanhe <MacHu-GWU@users.noreply.github.com>
2021-12-09 20:55:42 -0500

    fix readthedocs yaml

--- bin/container-only-build-lambda-layer.sh
+++ bin/container-only-build-lambda-layer.sh
@@ -3,8 +3,6 @@
 # NOTE: This script should be executed INSIDE of the container
 
 dir_here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#dir_bin="$(dirname "${dir_here}")"
-dir_bin="${dir_here}"
 dir_project_root="$(dirname "${dir_here}")"
 dir_build_lambda="${dir_project_root}/build/lambda"
 echo "build lambda layer"
