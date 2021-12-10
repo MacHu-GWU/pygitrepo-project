97c032725ba8ce539b285444489e3a1fc3051cf3
Sanhe <MacHu-GWU@users.noreply.github.com>
2021-12-09 19:37:13 -0500

    Update color_print.py

--- pygitrepo/color_print.py
+++ pygitrepo/color_print.py
@@ -1,52 +1,40 @@
 # -*- coding: utf-8 -*-
 
+"""
+Utility module for colored text print in terminal.
+"""
+
 import os
-from .pkg.mini_colorma import Fore, Back, Style
+
+from .pkg.mini_colorma import Fore, Style
 
 TAB = " " * 2
 
 
 def pgr_print(message):
     """
-    "[pygitrepo] ... message ... {Style.RESET_ALL}
+    Syntax sugar for:
+
+    [pygitrepo] ... message ... {Style.RESET_ALL}
+
+    :type message: str
     """
     print("[pygitrepo] {}{}".format(message, Style.RESET_ALL))
 
 
 def pgr_print_done(indent=0):
-    print("[pygitrepo] {}{}done{}".format(
-        Fore.CYAN,
-        indent * TAB,
-        Style.RESET_ALL,
-    ))
-
-
-def print_path(title, path):
     """
-    Print ``- virtual environment directory: /path-to/my_venv``
+    Syntax sugar for:
+
+    [pygitrepo] {number_of_tab_indent}done
+
+    :type indent: int
     """
-    if os.path.exists(path):
-        color = Fore.GREEN
-    else:
-        color = Fore.RED
-    msg = "{reset}- {cyan}{title}: {color}{path}{reset}".format(
-        reset=Style.RESET_ALL,
+    print("{reset}[pygitrepo] {cyan}{indent}done{reset}".format(
         cyan=Fore.CYAN,
-        color=color,
-        title=title,
-        path=path,
-    )
-    print(msg)
-
-
-def print_line(msg):
-    """
-    Color print a message reset style before and after.
-
-    :type msg: str
-    :rtype: str
-    """
-    print("{reset}{msg}{reset}".format(reset=Style.RESET_ALL, msg=msg))
+        reset=Style.RESET_ALL,
+        indent=indent * TAB,
+    ))
 
 
 def colorful_path(path):
@@ -63,3 +51,37 @@ def colorful_path(path):
         path=path,
         reset=Style.RESET_ALL,
     )
+
+
+def print_path(title, path):
+    """
+    Print a description of a path and the absolute path. If path exists,
+    path is in green, if not exists, path is in red.
+
+    Example: "- virtual environment directory: /path-to/my_venv"
+
+    :type title: str
+    :type path: str
+    """
+    if os.path.exists(path):
+        color = Fore.GREEN
+    else:
+        color = Fore.RED
+    msg = "{reset}- {cyan}{title}: {colorful_path}".format(
+        reset=Style.RESET_ALL,
+        cyan=Fore.CYAN,
+        color=color,
+        title=title,
+        colorful_path=colorful_path(path),
+    )
+    print(msg)
+
+
+def print_line(msg):
+    """
+    Color print a message reset style before and after.
+
+    :type msg: str
+    :rtype: str
+    """
+    print("{reset}{msg}{reset}".format(reset=Style.RESET_ALL, msg=msg))
